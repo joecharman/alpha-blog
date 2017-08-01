0c5c68d08e20c9361dc506095c406150d4b9b038
Aplha Blog <joe.charman@blueyonder.co.uk>
2017-08-01 20:57:59 +0000

    modify readme

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,5 +4,6 @@ class PagesController < ApplicationController
   end
   def about
   end 
-  
+  def contact
+  end
 end 
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
 root 'pages#home'
   
 get 'about', to: 'pages#about'
+get 'contact',  to: 'pages#contact'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
